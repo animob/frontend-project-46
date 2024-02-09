c42bde0df199d00b828f35640e993f0c5b0713ba
Anna Dubik <anngwork@gmail.com>
2024-02-09 21:19:13 +0300

    Add gendiff implement data parsing from files

---
+++ src/gendiff.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+import { readFileSync } from 'fs';
+
+const genDiff = (file1Path, file2Path, format1, format2) => {
+  const file1Content = fs.readFileSync(file1Path, 'utf-8');
+  const file2Content = fs.readFileSync(file2Path, 'utf-8');
+
+  const data1 = JSON.parse(file1Content);
+  const data2 = JSON.parse(file2Content);
+
+  console.log('Data from file 1:', data1);
+  console.log('Data from file 2:', data2);
+
+};
+
+export default genDiff;
